7ec043bdff2707d028625e33396f1c87334f61f8
subasinik <subasinik@thestackly.com>
2025-09-17 08:26:15 +0530

    DAY 6 TASK UPDATED

---
+++ #TASK DAY-6.py
@@ -0,0 +1,23 @@
+#TASK DAY-6
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+fruits.append("Pineapple")
+print(fruits)
+
+
+
+
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+fruits.remove("Banana")
+print(fruits)
+
+
+students = {"Amit", "Priya", "Rohan", "Sneha", "Amit"}
+print("Student names set:", students)
+
+
+
+numbers = (10, 20, 30, 40)
+print("Second element:", numbers[1])
+print("Last element:", numbers[-1])
